fcec3d3e94f304c3b96c3275d406b494b1783a80
Sam Robbins <samrobbinsgb@gmail.com>
2020-08-03 16:19:45 +0100

    Add file upload and remove default components

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { action } from "@storybook/addon-actions";
-import { Button } from "@storybook/react/demo";
-import "../styles/index.css";
+
 export default {
-	title: "Button",
-	component: Button,
+	title: "File Upload",
 };
 
-export const Text = () => (
-	<Button onClick={action("clicked")}>Hello Button</Button>
-);
-
-export const Emoji = () => (
-	<Button onClick={action("clicked")}>
-		<span className="bg-blue-600" role="img" aria-label="so cool">
-			hello
-		</span>
-	</Button>
+export const FileUpload = () => (
+	<div className="flex justify-center">
+		<label class="w-64 flex flex-col items-center px-2 py-2 bg-white text-gray-800 rounded-lg shadow  border border-gray-300 cursor-pointer hover:bg-gray-200">
+			<svg
+				class="w-8 h-8"
+				fill="currentColor"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+			>
+				<path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+			</svg>
+			<span class="mt-2 text-base leading-normal">Select a file</span>
+			<input type="file" class="hidden" />
+		</label>
+	</div>
 );
